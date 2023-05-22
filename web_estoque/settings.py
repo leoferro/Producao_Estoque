9c1f703c692b1940590bfee4509f818147c759f0
Leonardo <leo9266@gmail.com>
2023-05-21 21:00:04 -0300

    csrf

--- web_estoque/settings.py
+++ web_estoque/settings.py
@@ -53,6 +53,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://producaoestoque-production.up.railway.app/'
+]
+
 ROOT_URLCONF = 'web_estoque.urls'
 
 TEMPLATES = [
